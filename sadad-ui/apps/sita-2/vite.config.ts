bec70d42da11894e90207930bad0213cf3a496dc
A.rezaei <ah.rezaei.14@gmail.com>
2025-10-01 12:59:52 +0330

    add-react

---
+++ sadad-ui/apps/sita-2/vite.config.ts
@@ -0,0 +1,17 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/sita-2',
+  plugins: [react()],
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+}));
